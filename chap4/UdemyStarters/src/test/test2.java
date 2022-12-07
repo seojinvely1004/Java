c707710bb9646d0c841666040a578a10ad1878ce
seojinvely1004 <seojin0124@naver.com>
2022-12-07 20:06:08 +0900

    feat(Java) : 과제2 초를입력받아 시간,분,초로출력하기 기능 구현 완료

---
+++ chap4/UdemyStarters/src/test/test2.java
@@ -0,0 +1,17 @@
+package test;
+
+public class test2 {
+	//연산자만 사용하여 출력하기.//초를 시간,분,초로 출력하기.
+	public static void main(String[] args) {
+		//초->시간 60*60으로 나눈다
+		//분->초 60으로 나눈다
+		//초->60으로 나눈 나머지
+		int time = 10000;//초단위 시간.
+		int hour = time/(60*60);
+		int minute = time/60 - (hour*60);
+		int second = time%60;
+		System.out.println(time + "초는" + hour + "시간" + minute + "분" + second + "초입니다.");
+
+	}
+
+}
