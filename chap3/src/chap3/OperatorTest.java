070f933e57bca215e5db94f0c59fb594b23f67ad
seojinvely1004 <seojin0124@naver.com>
2022-12-07 15:14:29 +0900

    feat(Java) : 형변환

---
+++ chap3/src/chap3/OperatorTest.java
@@ -0,0 +1,16 @@
+package chap3;
+
+public class OperatorTest {
+
+	public static void main(String[] args) {
+		int avgInt = 10 / 3;//3
+		double avgDouble = 10.0 / 3.0; //3.3333333
+		double avgDouble2 = 10.0 / (double)3;//3.33333
+		
+		System.out.println(avgInt);
+		System.out.println(avgDouble);
+		System.out.println(avgDouble2);
+
+	}
+
+}
