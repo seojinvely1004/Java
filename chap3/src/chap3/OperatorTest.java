3c2de10ea7c0608d0ab690123ae7b4c5aa917e02
seojinvely1004 <seojin0124@naver.com>
2022-12-07 15:43:31 +0900

    feat(Java) : 연산자

--- chap3/src/chap3/OperatorTest.java
+++ chap3/src/chap3/OperatorTest.java
@@ -6,10 +6,17 @@ public class OperatorTest {
 		int avgInt = 10 / 3;//3
 		double avgDouble = 10.0 / 3.0; //3.3333333
 		double avgDouble2 = 10.0 / (double)3;//3.33333
-		
+
 		System.out.println(avgInt);
 		System.out.println(avgDouble);
 		System.out.println(avgDouble2);
+
+		//연산자
+		int a = 10;
+		int b = 20;
+		//a = a + b - 1 * 3 / 5;
+		//연산 우선순위 () : * / : +- : =
+		a += b; //a = a + b;와 같다. 
 
 	}
 
