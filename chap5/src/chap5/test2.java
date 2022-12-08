616e27779f7686cf4810e8b2a421bf9e27c16efd
seojinvely1004 <seojin0124@naver.com>
2022-12-08 17:53:10 +0900

    feat(Java) : 과제 기능 구현 완료 (solving)

--- chap5/src/chap5/test2.java
+++ chap5/src/chap5/test2.java
@@ -8,6 +8,22 @@ public class test2 {
 		 * 입력된 변수 갯수 오름차순으로 정렬 결과 출력한다.
 		 * > int형변환, 정렬(asc) 
 		 */
+		int[] arr = {45,89,100,23,234,567};
+		for(int i = 0; i<arr.length; i++) {
+			for(int j = 0; j <arr.length;j++) {
+				if(arr[i] < arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		for(int i = 0; i<arr.length; i++) {
+			System.out.println(arr[i]);
+			if(i + 1 != arr.length) {
+				System.out.println(",");
+			}
+		}
 
 	}
 
