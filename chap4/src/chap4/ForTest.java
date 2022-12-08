6d2f5302967664a2e475782ee9297e8b54a6de51
seojinvely1004 <seojin0124@naver.com>
2022-12-08 11:31:30 +0900

    feat(Java) : 중첩for문

--- chap4/src/chap4/ForTest.java
+++ chap4/src/chap4/ForTest.java
@@ -3,8 +3,19 @@ package chap4;
 public class ForTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		char star = '*';
 
+		int count = (int)(Math.random() * 20) + 1;
+		//count만큼 한줄에 *를 반복 출력 1~20난수
+		int lines = (int)(Math.random() * 10) + 1;
+		//출력할 줄 갯수 1~0난수
+		System.out.println(lines + "줄에" + count + "만큼 출력합니다.");
+		for(int j = 1; j <= lines; j++) {
+			for(int i = 1; i <=count; i++) {
+				System.out.print(star);
+			}//inner for end
+			System.out.println("\n");//inner for문 끝난 후 줄을 바꿔 lines가 for문되도록
+		}//outer for end
 	}
 
 }
