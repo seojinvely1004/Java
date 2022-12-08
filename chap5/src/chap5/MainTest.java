594026c48362b1a7ff3d3ccc9853352cc0aabe48
seojinvely1004 <seojin0124@naver.com>
2022-12-08 15:26:40 +0900

    feat(Java) : String배열,클래스,메서드/ 커맨드라인통해입력받기

--- chap5/src/chap5/MainTest.java
+++ chap5/src/chap5/MainTest.java
@@ -21,5 +21,32 @@ public class MainTest {
 		for(int i = 0; i < userarray.length; i++) {
 			System.out.println(userarray[i]);
 		}
+		//String 타입 입력 덧셈 100 45를 run configuration의 argument에 입력.
+		System.out.println(args[0] + args[1]);//10045
+		System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));//145
+		
+		//
+		String s1 = "java";
+		String s3 = new String("java");
+		String s4 = new String("java");
+		String s2 = "java";
+		s2 = "jsp";
+		//명령행매개변수, 문자열 1번 메모리 생성되면 고정이다.
+		
+		if(s1 == s2) {//true
+			System.out.println("s1 == s2 결과 같다");
+		}
+		if(s3 == s4) {//false
+			System.out.println("s3 == s4 결과 같다");
+		}
+		if(s1.equals(s2)) {//true
+			System.out.println("s1.equals(s2) 결과 같다");
+		}
+		if(s3.equals(s4)) {//true
+			System.out.println("s3.equals(s4)  결과 같다");
+		}
 	}
+
+	
 }
+
