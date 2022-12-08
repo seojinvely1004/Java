c06c53981de5913a9889182b90aa66972a7a6a2d
seojinvely1004 <seojin0124@naver.com>
2022-12-08 17:08:38 +0900

    feat(test): 과제1,2(unsolving)

---
+++ chap5/src/chap5/test1.java
@@ -0,0 +1,10 @@
+package chap5;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ chap5/src/chap5/test2.java
@@ -0,0 +1,14 @@
+package chap5;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		/**입력변수 (min5~max10개 라고가정한다.)
+		 * 45 89 100 23 234 567 
+		 * 입력된 변수 갯수 오름차순으로 정렬 결과 출력한다.
+		 * > int형변환, 정렬(asc) 
+		 */
+
+	}
+
+}
