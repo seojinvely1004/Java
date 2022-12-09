c920191c667c135c6236feba9f3276dd66e5bea7
seojinvely1004 <seojin0124@naver.com>
2022-12-09 13:17:20 +0900

    feat(Java):클래스.메서드선언, main에서 호출 (2)String

--- chap6/src/chap6/MethodTest.java
+++ chap6/src/chap6/MethodTest.java
@@ -8,6 +8,9 @@ class B{
 	int m2(int param){//1.m2메서드에 param이라는 매개변수를 주었다.
 		return param*10;
 	}
+	String m3(int param1, int param2){
+		return param1 * param2 + "결과를 리턴합니다.";
+	}
 }
 public class MethodTest {
 
@@ -17,5 +20,6 @@ public class MethodTest {
 		System.out.println(result1);//100//완성된 메서드를 main에서 호출해준다.//
 		System.out.println(b1.m2(5));//50 //2.위처럼 호출해줘도 되고, 바로 불러와도 된다.
 		System.out.println(b1.m2(50));//500
+		System.out.println(b1.m3(6, 8));//48결과를 리턴합니다.
 	}
 }
