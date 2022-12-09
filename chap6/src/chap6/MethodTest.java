932ff4e0626c438f0b9d95acf57ec80dc5683dfe
seojinvely1004 <seojin0124@naver.com>
2022-12-09 13:22:47 +0900

    feat(Java):클래스.메서드선언, main에서 호출 (3)배열

--- chap6/src/chap6/MethodTest.java
+++ chap6/src/chap6/MethodTest.java
@@ -1,5 +1,7 @@
 package chap6;
 
+import java.util.Arrays;
+
 class B{
 	int m1(){
 		int local = 10;
@@ -11,6 +13,14 @@ class B{
 	String m3(int param1, int param2){
 		return param1 * param2 + "결과를 리턴합니다.";
 	}
+	int[] m4(int[] ar) {
+		int []result = new int[ar.length];
+		//ar배열 각 요소에 *10 배열 저장 리턴
+		for(int i = 0; i < ar.length; i++) {
+			result[i] = ar[i] * 10;
+		}
+		return result;
+	}
 }
 public class MethodTest {
 
@@ -21,5 +31,8 @@ public class MethodTest {
 		System.out.println(b1.m2(5));//50 //2.위처럼 호출해줘도 되고, 바로 불러와도 된다.
 		System.out.println(b1.m2(50));//500
 		System.out.println(b1.m3(6, 8));//48결과를 리턴합니다.
+		int main_ar [] = {10,20,45,1,99};
+		int [] main_result = b1.m4(main_ar);
+		System.out.println(Arrays.toString(main_result));
 	}
 }
