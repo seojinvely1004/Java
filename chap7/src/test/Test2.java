94373552fb22a17c5b2cd44fede6a82288b85d74
seojinvely1004 <seojin0124@naver.com>
2022-12-14 09:57:44 +0900

    feat(Java) : Information, InformaionList > main의 매개변수 전달받아 배열 활용하기.

--- chap7/src/test/Test2.java
+++ chap7/src/test/Test2.java
@@ -18,6 +18,10 @@ class Product extends Information{
 		super(id, name);
 		this.price = price;
 	}
+	@Override
+	String print() {
+		return super.print() + ":" + name + ":" + price;
+	}
 	
 }
 class Board extends Information{//Board() {super();} 
@@ -30,9 +34,26 @@ class Board extends Information{//Board() {super();}
 		this.contents = contents;
 		this.viewcount = viewcount;
 	}
+	@Override
+	String print() {
+		return super.print() + ":" + name + ":" + contents + ":" + viewcount;
+	}
 	
 }
 class Member extends Information{
+	String email, phone;
+
+	Member(String id, String name, String email, String phone) {
+		super(id, name);
+		this.email = email;
+		this.phone = phone;
+		
+	}
+	@Override
+	String print() {
+		return super.print() + ":" + name + ":" + email + ":" + phone;
+	}
+	
 	
 }
 class InformationList{
@@ -41,10 +62,10 @@ class InformationList{
 	int count;//배열 인덱스 증가 담기 위한 변수
 	InformationList(String length){//생성자 명령행 매개변수/main으로부터 전달받아 입력받은 수만큼 배열length로 하여 배열객체생성
 		this.length = Integer.parseInt(length);
-		array = new Information(this.length);
+		array = new Information[this.length];
 	}
 	void add(Information inform) {//6번,﻿main으로부터 전달받은 각 객체들을 멤버변수 배열에 저장
-		if(count > length) {
+		if(count >= length) {
 			return;//add메서드 종료 (6번)
 		}
 		array[count++] = inform;
@@ -58,17 +79,13 @@ class InformationList{
 }
 public class Test2 {
 	public static void main(String[] args) {
-		InformationList list = new InformationList(args[0]);//
-		list.add(new Board("1","게시물1","현재 조회수 10입니다", 
-				10));
+		InformationList list = new InformationList(args[0]);
+		list.add(new Board("1","게시물1","현재 조회수 10입니다", 10));
 		list.add(new Product("100", "웅진비데", 300000));
-		list.add(new Member("hong","홍길동","hong@a.com","010-222-
-				2222"));
-				list.add(new Board("2","게시물2","새로운 게시물 추가합니다
-						", 0));
-						list.add(new Product("200", "웅진정수기", 1000000));
-				list.add(new Member("lee","이철수","chul@b.com","010-333-
-						3333"));
-						list.read();
+		list.add(new Member("hong","홍길동","hong@a.com","010-222-2222"));
+		list.add(new Board("2","게시물2","새로운 게시물 추가합니다", 0));
+		list.add(new Product("200", "웅진정수기", 1000000));
+		list.add(new Member("lee","이철수","chul@b.com","010-333-3333"));
+		list.read();
 	}
 }
