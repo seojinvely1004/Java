a09e0de19a93a84c76a22c32203e4c069c6fb282
seojinvely1004 <seojin0124@naver.com>
2022-12-19 10:35:30 +0900

    feat(Java) : isEmpty()사용하여 pop()호출하기

--- StandardPractice of Java/src/chap11/StackQueueTest.java
+++ StandardPractice of Java/src/chap11/StackQueueTest.java
@@ -9,10 +9,13 @@ public class StackQueueTest {
 		st.push(1);
 		st.push(2);
 		st.push(3);//push(): 데이터를저장한다
-		System.out.println(st.pop());//3 조회 후 '삭제'한다.
-		System.out.println(st.pop());//2
-		System.out.println(st.pop());//1 > stack에 데이터 없다.
-		System.out.println(st.pop());//Exception in thread "main" java.util.EmptyStackException
+		while(!st.isEmpty()) {//stack이 비어있지 않으면 pop을 반복해라. 
+			System.out.println(st.pop());
+		}
+		//System.out.println(st.pop());//3 조회 후 '삭제'한다.
+		//System.out.println(st.pop());//2
+		//System.out.println(st.pop());//1 > stack에 데이터 없다.
+		//System.out.println(st.pop());//Exception in thread "main" java.util.EmptyStackException
 	}
 
 }
