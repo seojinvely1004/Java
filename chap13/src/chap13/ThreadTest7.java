3f68f0252c2422b284d1cbae5988448081c893bd
seojinvely1004 <seojin0124@naver.com>
2022-12-20 14:45:32 +0900

    feat(Java) : wait(), notify()메서드로 synchronized

--- chap13/src/chap13/ThreadTest7.java
+++ chap13/src/chap13/ThreadTest7.java
@@ -1,13 +1,37 @@
 package chap13;
+//wait()통신하는것. notify()멈췄던 것을 동작해.
 class Buffer{//저장
 	int value;
+	boolean available = false;
+	//available false value 저장가능상태
 	synchronized int getValue() {
+		while(available == false) {//getValue시엔 available이 무조건 true여야한다. false면 기다려라
+			try {
+				wait();
+			}catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+		}
 		System.out.println("조회 : "+ value);
+		available = false;//읽어왔으므로 false로만들어줌
+		notify();//<<<<<==========================
 		return value;
 	}
 	synchronized void setValue(int value) {
+		while(available == true) {
+			try {
+				wait();//읽을 것이 없는데 읽을거면 wait해라.
+			}catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		//available == true
 		this.value = value;
+		available = true;
 		System.out.println("저장 : "+ value);
+		notify();//<<<<<==========================
 	}
 }
 class Producer extends Thread{//프로듀싱 동작(저장)
