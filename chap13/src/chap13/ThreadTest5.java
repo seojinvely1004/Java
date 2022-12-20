58a5aacb5c6b5042e427db0bc3be03a1e6c30f75
seojinvely1004 <seojin0124@naver.com>
2022-12-20 13:25:14 +0900

    feat(Java) : join메서드는 지정된 시간동안 쓰레드가 실행되도록 한다

--- chap13/src/chap13/ThreadTest5.java
+++ chap13/src/chap13/ThreadTest5.java
@@ -11,8 +11,14 @@ public class ThreadTest5 {
 		JoinThread t = new JoinThread();
 		//run 수행 
 		t.start();//즉각적으로 run실행이 안될 수도 있다. 자체로 run실행하라는 뜻이 아닌 run이 가능한 상태로 만들라는 의미이다. 
-		//자바 스레드 스케줄러에 따라 다르다 .//[run메서드내부 = 10]
-		System.out.println(t.i);//[1] _ run메서드수행 전
+		//자바 스레드 스케줄러에 따라 다르다 .>>t : 실행'가능'상태.main:실행상태//[run메서드내부 = 10]
+		try {
+			t.join();//t:실행상태 / main:일신중단상태(main은일단빠지고 t를 기다린다)
+		}catch(Exception e) {
+			e.printStackTrace();
+		}		
+		//System.out.println(t.i);//[1] _ run메서드수행 전
+		System.out.println("main메소드 내부 = " + t.i);
 		System.out.println("main 종료");
 	}
 
