3d5e1b4bb7a3581cb9712ef2d187c3a3e1c8c2f1
seojinvely1004 <seojin0124@naver.com>
2022-12-20 13:18:42 +0900

    feat(Java) : JoinThread객체생성

---
+++ chap13/src/chap13/ThreadTest5.java
@@ -0,0 +1,19 @@
+package chap13;
+class JoinThread extends Thread{
+	int i = 1; 
+	public void run() {//run이 수행됬냐안됬냐에 따라 i의값은 달라진다.
+		i = 10;
+		System.out.println("run메서드내부 = " + i);
+	}
+}
+public class ThreadTest5 {
+	public static void main(String[] args) {
+		JoinThread t = new JoinThread();
+		//run 수행 
+		t.start();//즉각적으로 run실행이 안될 수도 있다. 자체로 run실행하라는 뜻이 아닌 run이 가능한 상태로 만들라는 의미이다. 
+		//자바 스레드 스케줄러에 따라 다르다 .//[run메서드내부 = 10]
+		System.out.println(t.i);//[1] _ run메서드수행 전
+		System.out.println("main 종료");
+	}
+
+}
