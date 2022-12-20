da079aaa50dc08d06a1c8a601da117c40919dfac
seojinvely1004 <seojin0124@naver.com>
2022-12-20 14:35:42 +0900

    feat(Java) : getValue가 1~10을 조회하는 동안 setValue는 10을저장

--- chap13/src/chap13/ThreadTest7.java
+++ chap13/src/chap13/ThreadTest7.java
@@ -6,23 +6,30 @@ class Buffer{//저장
 		return value;
 	}
 	synchronized void setValue(int value) {
+		this.value = value;
 		System.out.println("저장 : "+ value);
 	}
 }
 class Producer extends Thread{//프로듀싱 동작(저장)
+	Buffer buf;
 	Producer(Buffer buf){//생성자
-		
+		this.buf = buf;
 	}
 	public void run(){ //스레드를 상속받았으니 run()메서드는 기본
-		
+		for(int i = 1; i <= 10; i++) {
+			buf.setValue(i);
+		}
 	}
 }
 class Consumer extends Thread{//Buffer값을 읽는 Consumer가 동작(조회)
+	Buffer buf;
 	Consumer(Buffer buf){//생성자
-		
+		this.buf = buf;
 	}
 	public void run() {
-		
+		for(int i = 1; i <= 10; i++) {
+			buf.getValue();
+		}	
 	}
 }
 public class ThreadTest7 {
