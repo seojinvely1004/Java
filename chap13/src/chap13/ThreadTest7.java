291322bb5651ea9017f4d6beb08319ea74ef3a8f
seojinvely1004 <seojin0124@naver.com>
2022-12-20 14:29:32 +0900

    feat(Java): 큰 틀 (초기세팅)2

--- chap13/src/chap13/ThreadTest7.java
+++ chap13/src/chap13/ThreadTest7.java
@@ -1,12 +1,29 @@
 package chap13;
 class Buffer{//저장
 	int value;
+	synchronized int getValue() {
+		System.out.println("조회 : "+ value);
+		return value;
+	}
+	synchronized void setValue(int value) {
+		System.out.println("저장 : "+ value);
+	}
 }
 class Producer extends Thread{//프로듀싱 동작(저장)
-	
+	Producer(Buffer buf){//생성자
+		
+	}
+	public void run(){ //스레드를 상속받았으니 run()메서드는 기본
+		
+	}
 }
 class Consumer extends Thread{//Buffer값을 읽는 Consumer가 동작(조회)
-	
+	Consumer(Buffer buf){//생성자
+		
+	}
+	public void run() {
+		
+	}
 }
 public class ThreadTest7 {
 
