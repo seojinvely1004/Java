5ce53443140e9b087939a6a7a20c4f482ede43c3
seojinvely1004 <seojin0124@naver.com>
2022-12-21 10:32:49 +0900

    feat(Java) : FileInputStream

---
+++ StandardPractice of Java/src/chap15/FileViewer.java
@@ -0,0 +1,26 @@
+package chap15;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileViewer {
+
+	public static void main(String[] args) {
+		try {
+
+			FileInputStream fis = new FileInputStream("c:\\test\\a.java");
+			while(true) {
+				int data = fis.read();//4byte중 1byte만 활용
+				if(data == -1) {break;}
+				System.out.println(data);//파일 입력 내용을 모니터 출력
+			}
+		}
+		catch(FileNotFoundException e) {
+			System.out.println("해당 파일은 존재하지 않습니다.");
+		}
+		catch(IOException e) {
+			System.out.println("파일 입력 중 오류가 발생했습니다.");
+		}
+	}
+}
