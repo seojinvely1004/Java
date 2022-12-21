7e5c243833c1c18a330eb14f5953d879ac0c90e8
seojinvely1004 <seojin0124@naver.com>
2022-12-21 10:39:45 +0900

    file(Java) : FileInputStream_IOException

--- StandardPractice of Java/src/chap15/FileViewer.java
+++ StandardPractice of Java/src/chap15/FileViewer.java
@@ -8,18 +8,21 @@ public class FileViewer {
 
 	public static void main(String[] args) {
 		try {
-			FileInputStream fis = new FileInputStream("c:\\test\\a.java");
+			FileInputStream fis = new FileInputStream("C:\\Users\\HostAd\\Desktop");
 			while(true) {
 				int data = fis.read();//4byte중 1byte만 활용
 				if(data == -1) {break;}
-				System.out.println(data);//파일 입력 내용을 모니터 출력
+				System.out.println((char)data);//파일 입력 내용을 모니터 출력
 			}
 		}
-		catch(FileNotFoundException e) {
-			System.out.println("해당 파일은 존재하지 않습니다.");
-		}
+		//catch(FileNotFoundException e) {
+		//	System.out.println("해당 파일은 존재하지 않습니다.");
+		//}
+		//catch(IOException e) {
+			//System.out.println("파일 입력 중 오류가 발생했습니다.");
+		//}
 		catch(IOException e) {
-			System.out.println("파일 입력 중 오류가 발생했습니다.");
+			e.printStackTrace();
 		}
 	}
 }
