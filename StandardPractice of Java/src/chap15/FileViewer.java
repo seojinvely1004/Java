966e4789ae33b297eadb4935f835cf8abb260a83
seojinvely1004 <seojin0124@naver.com>
2022-12-21 10:47:33 +0900

    feat(Java): FileInputStream로 현재경로읽어오고 fis.close해보기

--- StandardPractice of Java/src/chap15/FileViewer.java
+++ StandardPractice of Java/src/chap15/FileViewer.java
@@ -8,12 +8,14 @@ public class FileViewer {
 
 	public static void main(String[] args) {
 		try {
-			FileInputStream fis = new FileInputStream("C:\\Users\\HostAd\\Desktop");
+			//FileInputStream fis = new FileInputStream("C:\\Users\\HostAd\\Desktop");
+			FileInputStream fis = new FileInputStream("src/chap15/FileViewer.java");
 			while(true) {
 				int data = fis.read();//4byte중 1byte만 활용
 				if(data == -1) {break;}
 				System.out.println((char)data);//파일 입력 내용을 모니터 출력
 			}
+			fis.close();
 		}
 		//catch(FileNotFoundException e) {
 		//	System.out.println("해당 파일은 존재하지 않습니다.");
