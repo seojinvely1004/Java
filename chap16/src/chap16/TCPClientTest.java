4d8f153998f21f5a75a4550dbf64dc77fb865886
seojinvely1004 <seojin0124@naver.com>
2022-12-22 15:20:42 +0900

    feat(Java) : TCP Server & Client

--- chap16/src/chap16/TCPClientTest.java
+++ chap16/src/chap16/TCPClientTest.java
@@ -1,12 +1,43 @@
 package chap16;
 
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class TCPClientTest {//2. 클라이언트 접속
 
 	public static void main(String[] args) throws Exception{
+		System.out.println("상품 정보를 입력하세요");
+		Scanner key = new Scanner(System.in);//키보드로부터 입력을 연결
+		System.out.println("상품명 : ");
+		String name = key.next();//공백 엔터 구분자(제외)
+		System.out.println("가격 : ");
+		String price = key.next();
+		System.out.println("수량 : ");
+		String balance = key.next();
+		
 		Socket s = new Socket("127.0.0.1", 10000);
-		System.out.println("====클라이언트가 서버로 접속하였습니다===");
+		String serverip = s.getInetAddress().getHostAddress();
+	
+		System.out.println("====클라이언트가" + serverip + " 서버로 접속하였습니다===");
+		OutputStream os = s.getOutputStream();
+		//String request = name + "-" price + "-" + balance;
+		//byte [] by = request.getBytes();
+ 	    //os.write(by);
+ 	    
+		name = name + " ";
+		price = price + " ";
+		balance = balance + " ";
+		os.write(name.getBytes());
+		os.write(price.getBytes());
+		os.write(balance.getBytes());
+		//////////////////////////////////
+		InputStream is = s.getInputStream();
+		
+		Scanner sc = new Scanner(is);
+		String response = sc.nextLine();
+		System.out.println("[클라이언트가 서버로부터" + response + "응답을 전달받았습니다.]");
 		s.close();
 		System.out.println("====클라이언트가 서버의 접속을 해제하였습니다====");
 
--- chap16/src/chap16/TCPServerTest.java
+++ chap16/src/chap16/TCPServerTest.java
@@ -1,18 +1,51 @@
 package chap16;
 
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class TCPServerTest {//1.서버실행_클라이언트의 접속을 기다린다.
 
 	public static void main(String[] args) throws Exception {
+		FileWriter fw = new FileWriter("product.txt");
 		ServerSocket ss = new ServerSocket(10000);
 		System.out.println("[서버는 10000 포트에서 대기중입니다]");
-		Socket s = ss.accept();
-		System.out.println("[서버는 클라이언트 접속 허용합니다]");
-		s.close();
-		System.out.println("[서버는 클라이언트 접속 해제합니다]");
-
+		int cnt = 0;
+		while(true) {
+			Socket s = ss.accept();//실행x/10/....
+			String clientip = s.getInetAddress().getHostAddress();
+			System.out.println("[서버는 " + clientip + "클라이언트 접속 허용합니다]");
+			cnt++;
+			if(cnt == 3) { break;}
+			//클라이언트 요청청받는다 = 입력스트림
+		    //BufferedReader br = new BufferedReader(new InputStreamReader(is)); 			
+			//String request = br.readLine();
+			//Scanner sc = new Scanner(is);
+		    //String request = sc.nextLine();
+			
+			//클라이언트 3개 " " = 입력스트림
+			InputStream is = s.getInputStream();
+			Scanner sc = new Scanner(is);
+			String name = sc.next();
+			String price = sc.next();
+			String balance = sc.next();
+			fw.write(name + " - " + price + " - " + balance);
+			
+		    String response = "추가 완료되었습니다";
+		    OutputStream os = s.getOutputStream();
+		    byte[] by = response.getBytes();
+		    os.write(by);
+ 		    System.out.println("[서버는 클라이언트로 " + "응답을 전달합니다.]");
+			//////////////////////////////////
+			fw.close();
+			is.close();
+			os.close();
+ 		    s.close();
+			System.out.println("[서버는 클라이언트 접속 해제합니다]");
+		}
 	}
 
 }
