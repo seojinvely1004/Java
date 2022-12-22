5a4a95cf0d60f9ee57ff4748546459e030d9c518
seojinvely1004 <seojin0124@naver.com>
2022-12-22 11:00:01 +0900

    feat(Java):URL Class

--- chap16/src/chap16/URLTest2.java
+++ chap16/src/chap16/URLTest2.java
@@ -1,6 +1,7 @@
 package chap16;
 
 import java.io.BufferedReader;
+import java.io.FileWriter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -10,6 +11,11 @@ public class URLTest2 {
 	public static void main(String[] args) {
 		try {
 			URL url = new URL("http://localhost:8080/html/board.html");
+			String filepath = url.getFile();
+			String filename = filepath.split("/")[filepath.split("/").length-1];
+			//boardlist.html 저장 = 출력
+			FileWriter fw = new FileWriter(filename);
+			
 			InputStream is = url.openStream();
 			InputStreamReader ir = new InputStreamReader(is);
 			BufferedReader br = new BufferedReader(ir);
@@ -17,8 +23,10 @@ public class URLTest2 {
 				String line = br.readLine();
 				if(line == null)break;
 				System.out.println(line);
+				fw.write(line + "\n");
 			}
 			br.close();
+			fw.close();
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
