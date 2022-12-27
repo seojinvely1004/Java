2a02ef898fda5c6a1c6425a00f385e1a214ad2d3
seojinvely1004 <seojin0124@naver.com>
2022-12-27 11:03:29 +0900

    feat(mariaDB) SELECT CASE ELSE

--- SQL/src/SUBQUERY IN_ANY.sql
+++ SQL/src/SUBQUERY IN_ANY.sql
@@ -61,6 +61,35 @@ ORDER BY department_id;
 SELECT salary, department_id,
 (SELECT AVG(salary) FROM employees WHERE e.department_id = department_id)
 FROM employees e
-WHERE salary > ANY (SELECT AVG(salary) FROM employees GROUP BY department_id);								
+WHERE salary > ANY (SELECT AVG(salary) FROM employees GROUP BY department_id);	
+
+-- alias AVG_SAL, SAL_TBL
+-- 1000 이상 급여 평균
+SELECT AVG(SALARY)
+FROM employees
+WHERE SALARY >= 10000; -- 12,632.~
+
+-- 회사 DB 부서별 권한 다르게
+SELECT SAL_TBL.AVG_SAL AS 고액월급평균
+FROM (SELECT AVG(SALARY) AVG_SAL FROM employees WHERE SALARY >= 10000) SAL_TBL;  -- 12,632.~
+
+-- 급여 수준에 따른 직급 조회
+-- EMPLOYEES 테이블 급여컬럼O , 직급컬럼 X
+-- 직급만들기 )  20000이상  임원, 15000 이상 부장, 10000이상 과장 , 5000이상 대리, 이하는 사원
+-- 급여 SALARY + SALARY * commition_pct
+SELECT MAX(SALARY), MIN(SALARY) FROM employees;
+
+
+SELECT first_name,
+CASE
+WHEN SALARY + SALARY * COMMISSION_PCT >= 20000 THEN '임원'
+WHEN SALARY + SALARY * COMMISSION_PCT >= 15000 THEN '부장'
+WHEN SALARY + SALARY * COMMISSION_PCT >= 10000 THEN '과장'
+WHEN SALARY + SALARY * COMMISSION_PCT >= 5000 THEN '대리'
+ELSE '사원'
+END 직급 
+FROM employees;
+
+SELECT first_name, salary, commission_pct FROM employees;
 								
 								
