a8782209626b573e1b10b61c055c4d37b2f9d357
seojinvely1004 <seojin0124@naver.com>
2022-12-27 10:34:42 +0900

    feat(mariaDB) : subQuery중첩

--- SQL/src/SUBQUERY IN_ANY.sql
+++ SQL/src/SUBQUERY IN_ANY.sql
@@ -31,3 +31,22 @@ SELECT FIRST_NAME, SALARY, DEPARTMENT_ID
 FROM EMPLOYEES
 WHERE (DEPartment_id, SALARY) IN (SELECT DEPARTMENT_ID, MAX(SALARY) FROM employees GROUP BY DEPARTMENT_ID)
 ORDER BY DEPARTMENT_ID;
+
+-- /////////subquery중첩 - 테이블 여러개 필요////
+-- locations 테이블 : 부서 위치 도시 정보
+-- departments 테이블 : 부서이름 부서장 도서코드(location에서찾아)
+-- employees 테이블 : 사원정보 
+-- 런던 도시 근무 사원명 조회
+-- 1. 런던 도시코드 조회(2400)
+SELECT location_id FROM locations WHERE city = 'london';  -- 24000
+-- 2. 런던 도시코드(2400)과 같은 도시코드 부서코드를 조회(40)
+SELECT * FROM departments;
+SELECT department_id FROM departments WHERE location_id = 2400; -- 40
+
+-- 3.
+SELECT first_name, department_id FROM employees
+WHERE department_id =
+							(SELECT department_id FROM departments WHERE location_id=
+								(SELECT location_id FROM locations WHERE city = 'south san francisco' )
+								);
+								
