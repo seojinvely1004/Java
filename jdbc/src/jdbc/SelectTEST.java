40cd074e1f903681429b723adacf085417819bcc
seojinvely1004 <seojin0124@naver.com>
2022-12-29 13:34:54 +0900

    feat(mariaDB) : select / resultset의 실제 구현된 클래스 보기 (completeResult)

---
+++ jdbc/src/jdbc/SelectTEST.java
@@ -0,0 +1,46 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SelectTEST {
+
+	public static void main(String[] args) {
+		Connection con = null;
+		try {
+			Class.forName(ConnectionInform.DRIVER_CLASS);// 0. JDBC Driver 호출
+			con = DriverManager.getConnection(ConnectionInform.JDBC_URL, ConnectionInform.USERNAME,
+					ConnectionInform.PASSWORD);// 1. DB연결
+			System.out.println("연결성공");
+			System.out.println(con.getAutoCommit()); // TRUE;
+
+			Statement st = con.createStatement(); // sql저장 - 전송객체 // emp_copy--> 100 길동 홍 1000 now() 50
+
+			/*
+			 * Scanner key = new Scanner(System.in); System.out.println("상품명 : "); String
+			 * name = key.nextLine(); System.out.println("가격 : "); int price =
+			 * key.nextInt(); System.out.println("수량 : "); int balance = key.nextInt();
+			 */
+
+			String sql = "select * from employees";// 107개레코드 * 11컬럼
+			ResultSet rs = st.executeQuery(sql);// select할때쓰는메서드, 리던은 resultset타입(행과열의일차원적인테이블구조)
+			// int rowcount = st.executeUpdate(sql); //insert할때쓰는메서드임
+			System.out.println("삽입행의 갯수=" + rs);
+
+			con.close();
+			System.out.println("연결 해제 성공");
+		} catch (ClassNotFoundException e) {
+			System.out.println("해당 드라이버가 발견되지 않았습니다.");
+		} catch (SQLException e) {
+			System.out.println("연결 정보를 확인하세요");
+		} finally {
+			try {
+				con.close();// 3. preference > library > add jar > 다운 한 드라이버 연결.
+			} catch (SQLException e) {
+			}
+		}
+	}
+}
