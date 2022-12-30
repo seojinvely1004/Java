9c173551f83c913112aaa67cec58d0c5c605cebd
seojinvely1004 <seojin0124@naver.com>
2022-12-30 13:58:35 +0900

    feat(crud) : 전체회원조회 위해 회원정보조회(본인) 추가

--- jdbc/src/member_crud/MemberMain.java
+++ jdbc/src/member_crud/MemberMain.java
@@ -10,7 +10,8 @@ public class MemberMain {
 			System.out.println("1.회원정보입력");
 			System.out.println("2.회원정보수정");
 			System.out.println("3.회원탈퇴");
-			System.out.println("4.전체회원조회");
+			System.out.println("4.회원정보조회");// id, pw입력하면 이름~주소 조회
+			System.out.println("5.전체회원조회");// 모든 회원 id, 이름
 			System.out.println("5.종료");
 			System.out.print("번호 입력 : ");
 			Scanner key = new Scanner(System.in);
