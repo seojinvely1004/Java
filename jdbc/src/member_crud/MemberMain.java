a362b8cacaa9960e41381af5dcedf8f13cc76d32
seojinvely1004 <seojin0124@naver.com>
2022-12-30 10:52:03 +0900

    feat(crud) : 첫화면 구성

---
+++ jdbc/src/member_crud/MemberMain.java
@@ -0,0 +1,25 @@
+package member_crud;
+
+import java.util.Scanner;
+
+public class MemberMain {
+
+	public static void main(String[] args) {
+		while (true) {
+			System.out.println("=== 회원관리 프로그램 ===");
+			System.out.println("1.회원정보입력");
+			System.out.println("2.회원정보수정");
+			System.out.println("3.회원탈퇴");
+			System.out.println("4.전체회원조회");
+			System.out.println("5.종료");
+			System.out.print("번호 입력 : ");
+			Scanner key = new Scanner(System.in);
+			int menu = key.nextInt();
+			if (menu == 5) {
+				System.out.println("프로그램을 종료합니다.");
+				break;
+			}
+		} // while
+	}// main
+
+}// class
