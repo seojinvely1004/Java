b3149163446763928308bfd55a9fcf7608a9a47a
seojinvely1004 <seojin0124@naver.com>
2023-01-13 10:04:42 +0900

    feat(MVC) : 어노테이션으로 바꿔보기

---
+++ first/src/main/java/annotation/mvc/HelloController.java
@@ -0,0 +1,31 @@
+package annotation.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+@Controller /*implements Controller 대신.*/
+public class HelloController {
+	@RequestMapping("/hello")
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		HelloDTO dto = new HelloDTO();
+		dto.setMessage("컨트롤러가 view로 전달하는 모델입니다.");//jsp view를통해 보여줄 것임
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("model",dto);//request.setAttribute를 대신하는표현.
+		mv.setViewName("hello");
+		
+		return mv;
+	}
+/*	@Override //nonspring  
+	public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+		//추가파라미터 request통해 읽어들어옴
+		HelloDTO dto = new HelloDTO();
+		dto.setMessage("컨트롤러가 view로 전달하는 모델입니다.");//jsp view를통해 보여줄 것임
+		request.setAttribute("model", dto);//request로 넣어놔야 jsp로 포워드되면서가져간다
+		//view 설정
+		return "hello.jsp";//${model.message}
+	}*/
+
+}
