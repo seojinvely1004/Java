153c9669551c7311d8385ea9d18da74caf131fea
seojinvely1004 <seojin0124@naver.com>
2023-01-18 14:13:04 +0900

    feat(jQuery) : ajax요청하고 @Responsebody요청응답받기

--- first/src/main/java/ajax/LoginController.java
+++ first/src/main/java/ajax/LoginController.java
@@ -3,6 +3,7 @@ package ajax;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
@@ -10,8 +11,17 @@ public class LoginController {
 	public String loginajax() {
 		return "ajax/loginform";
 	}
-	@PostMapping("loginajax")
+	@ResponseBody
+	@PostMapping(value="/loginajax",produces = {"application/json;charset=utf-8"})
 	public String loginajax2(String id, String pw) {
-		return "ajax/loginprocess";
+		String result = null;
+		if(id.equals("ajax") && pw.equals("1234")){
+			result = "login성공";
+		}else {
+			result = "login실패";
+		}
+		return "{ \"process\" : \"" + result + "\" }";//형식은 json이지만 자바입장에선 String이다,변수이름이 process
+		//return result; 서버자체에이상은없지만 client가받을때 json형태아니라 못받는다
+		
 	}
 }
