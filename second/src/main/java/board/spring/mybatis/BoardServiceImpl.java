f9f4820e11c3e91afddc021873a986a03df92e52
seojinvely1004 <seojin0124@naver.com>
2023-01-20 14:37:02 +0900

    refeat(service)

--- second/src/main/java/board/spring/mybatis/BoardServiceImpl.java
+++ second/src/main/java/board/spring/mybatis/BoardServiceImpl.java
@@ -2,20 +2,20 @@ package board.spring.mybatis;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 @Service
 public class BoardServiceImpl implements BoardService {
-
+	@Autowired
+	BoardDAO dao;
 	@Override
 	public int getTotalBoard() {
-		// TODO Auto-generated method stub
-		return 0;
+		return dao.getTotalBoard();
 	}
 
 	@Override
 	public List<BoardDTO> getBoardList(int limit) {
-		// TODO Auto-generated method stub
-		return null;
+		return dao.getBoardList(limit);
 	}
 
 }
